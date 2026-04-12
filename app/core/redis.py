108d77e39d09514980ca9b6f21be4619b00e6ab4
Stimpik <iderik@mail.ru>
2026-04-12 14:27:38 +0300

    Add redis.py

---
+++ app/core/redis.py
@@ -0,0 +1,14 @@
+import redis.asyncio as redis
+from .config import settings
+
+_redis_client = None
+
+def get_redis_client():
+    global _redis_client
+    if _redis_client is None:
+        _redis_client = redis.Redis(
+            host=settings.REDIS_HOST,
+            port=settings.REDIS_PORT,
+            decode_responses=True,
+        )
+    return _redis_client
